88268e34948fd402738d05e60a084042ea09ea99
pengppp <89523167+pengppp@users.noreply.github.com>
2021-09-29 10:11:54 +0400

    Add files via upload

---
+++ ghg-cdn/js/config.js
@@ -0,0 +1,15 @@
+const config={
+	"liveChat":["000"],//客服号，根据格式填写 
+	"ios_download":"https://z8d31.sm00009.com:1818/qpiVRX0rF",  //苹果下载链接
+	"android_download":"apk/97-24a.apk"  //安卓下载链接
+}
+
+
+//百度统计miliao
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ecfe00af5566727e403c3e8bafb5073c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
