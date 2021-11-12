220c818e2111fb6302604424f29bc749d3cc8d79
pengppp <89523167+pengppp@users.noreply.github.com>
2021-11-12 15:22:27 +0800

    Add files via upload

---
+++ yr4-cdn/data.js
@@ -0,0 +1,42 @@
+
+// 配置联系号码与统计在这里改
+// 如果需要增加号码，按下面的格式添加即可
+
+// 联系号码可以添加任意多个，cnzz只能添加一条
+
+// title 在http://www.jsons.cn/unicode/此处理转码  --- 中文转换&#XXXX
+
+const data = [
+  { key: "qq", value: "256377" }, 
+  { key: "qq", value: "060810" }, 
+  { key: "qq", value: "586778" }, 
+  { key: "qq", value: "357741" }, 
+  { key: "qq", value: "993377" }, 
+  { key: "qq", value: "565655" }, 
+  { key: "qq", value: "198198" }, 
+  { key: "qq", value: "566262" }, 
+  { key: "qq", value: "996777" }, 
+  { key: "qq", value: "885885" }, 
+  { key: "qq", value: "418418" }, 
+  { key: "qq", value: "002919" }, 
+  { key: "qq", value: "362648" }, 
+  {
+    key: "androidLink",
+    value: "https://yc-001.oss-accelerate.aliyuncs.com/goodluck1103.apk",
+  },
+  {
+    key: "iosLink",
+    value: "https://w1sds.top:22045/ba704afd",
+  },
+  {
+    key: "title",
+    value: "同城约炮",
+  },
+];
+
+// cdn资源加速链接
+// const cdndomain = 'https://cdn.jsdelivr.net/gh/pengppp/fast/yr2-cdn'
+const cdndomain = '.'
+
+// 统计代码至 index.html  head标签或者body标签中进行添加
+// <div style="display: none;"><script src="https://s4.cnzz.com/z_stat.php?id=1280182156&web_id=1280182156"></script></div>
