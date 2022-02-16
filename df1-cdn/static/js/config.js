c4f890409b48cc6c38efa18a1e7ea3b5e2183955
pengppp <89523167+pengppp@users.noreply.github.com>
2022-02-16 12:48:49 +0400

    Add files via upload

---
+++ df1-cdn/static/js/config.js
@@ -0,0 +1,14 @@
+const config={
+	"liveChat":["555333","000444","360360","000555","777666","888999","333111","520520","555888","019018","999111","168168","111222","007007","100100","999555","111333"],//客服号，根据格式填写 
+	"ios_download":"https://e2zxb.cjjzgc.com:1819/qj4Nhb2tE",  //苹果下载链接
+	"android_download":"apk/yy.apk"  //安卓下载链接
+}
+
+//百度统计 总
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c2058c4f30e74660257ab9d17cde8ee6";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
